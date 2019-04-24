6d1704b05819c01e1bd7f1d2d165e8ea298db80f
MathengeNewton <45616070+MathengeNewton@users.noreply.github.com>
2019-04-25 02:10:18 +0300

    Add files via upload

---
+++ potfolio/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<head>
+    <title>
+        newton
+    </title>
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+</head>
+
+<body>
+    <nav>
+        <div class="logo">
+            <h4>my folio</h4>
+        </div>
+        <ul class="nav-links">
+            <li>
+                <a href="#">welcome</a>
+            </li>
+            <li>
+                <a href="#">about</a>
+            </li>
+            <li>
+                <a href="#">work</a>
+            </li>
+            <li>
+                <a href="#">projects</a>
+            </li>
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+    </nav>
+</body>
+
+</html>
